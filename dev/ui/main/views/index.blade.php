259242d59a8957ab4573b4502b2938f964b47571
khai | apikey:1b0dd90a412730e44d310c9311dc8e297af006c00015f3e87f8c47706999e9e6 <khai.nh@fsofts.com>
2024-10-12 00:25:53 +0700

    save

--- dev/ui/main/views/index.blade.php
+++ dev/ui/main/views/index.blade.php
@@ -1,38 +1,29 @@
-  <span id="forkongithub">
-    <a href="https://github.com/szimek/signature_pad">Fork me on GitHub</a>
-  </span>
 
-  <div id="signature-pad" class="signature-pad">
-    <div id="canvas-wrapper" class="signature-pad--body">
-      <canvas>
-      </canvas>
+<div id="signature-pad" class="signature-pad">
+<div id="canvas-wrapper" class="signature-pad--body">
+    <canvas width="660" style="touch-action: none; user-select: none;" height="200"></canvas>
+</div>
+<div class="signature-pad--footer">
+    <div class="description">Sign above</div>
+
+    <div class="signature-pad--actions">
+    <div class="column">
+        <button type="button" class="button clear" data-action="clear">Clear</button>
+        <button type="button" class="button" data-action="undo" title="Ctrl-Z">Undo</button>
+        <button type="button" class="button" data-action="redo" title="Ctrl-Y">Redo</button>
+        <br/>
+        <button type="button" class="button" data-action="change-color">Change color</button>
+        <button type="button" class="button" data-action="change-width">Change width</button>
+        <button type="button" class="button" data-action="change-background-color">Change background color</button>
+
     </div>
-    <div class="signature-pad--footer">
-      <div class="description">Sign above</div>
-
-      <div class="signature-pad--actions">
-        <div class="column">
-          <button type="button" class="button clear" data-action="clear">Clear</button>
-          <button type="button" class="button" data-action="undo" title="Ctrl-Z">Undo</button>
-          <button type="button" class="button" data-action="redo" title="Ctrl-Y">Redo</button>
-          <br/>
-          <button type="button" class="button" data-action="change-color">Change color</button>
-          <button type="button" class="button" data-action="change-width">Change width</button>
-          <button type="button" class="button" data-action="change-background-color">Change background color</button>
-
-        </div>
-        <div class="column">
-          <button type="button" class="button save" data-action="save-png">Save as PNG</button>
-          <button type="button" class="button save" data-action="save-jpg">Save as JPG</button>
-          <button type="button" class="button save" data-action="save-svg">Save as SVG</button>
-          <button type="button" class="button save" data-action="save-svg-with-background">Save as SVG with
-            background</button>
-        </div>
-      </div>
-
-      <div>
-        <button type="button" class="button" data-action="open-in-window">Open in Window</button>
-      </div>
+    <div class="column">
+        <button type="button" class="button save" data-action="save-png">Save as PNG</button>
+        <button type="button" class="button save" data-action="save-jpg">Save as JPG</button>
+        <button type="button" class="button save" data-action="save-svg">Save as SVG</button>
+        <button type="button" class="button save" data-action="save-svg-with-background">Save as SVG with
+        background</button>
+    </div>
     </div>
   </div>
 
