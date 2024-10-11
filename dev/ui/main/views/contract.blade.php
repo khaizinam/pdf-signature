7f1ee4dbe0637cb81a5c95bd0bf67f0f503c9722
khai | apikey:1b0dd90a412730e44d310c9311dc8e297af006c00015f3e87f8c47706999e9e6 <khai.nh@fsofts.com>
2024-10-12 05:47:14 +0700

    save

--- dev/ui/main/views/contract.blade.php
+++ dev/ui/main/views/contract.blade.php
@@ -171,8 +171,6 @@
                 height: 50,
             });
         })
-
-
         const pdfBytes = await pdfDoc.save();
         const blob = new Blob([pdfBytes], { type: 'application/pdf' });
         const url = URL.createObjectURL(blob);
