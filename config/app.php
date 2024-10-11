72c7bb784b1708fd4e997d563d53369e8fe162f9
khai | apikey:1b0dd90a412730e44d310c9311dc8e297af006c00015f3e87f8c47706999e9e6 <khai.nh@fsofts.com>
2024-10-12 02:55:08 +0700

    save

--- config/app.php
+++ config/app.php
@@ -360,7 +360,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Ho_Chi_Minh'),
 
     /*
     |--------------------------------------------------------------------------
@@ -373,11 +373,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'vi'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'vi'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'vi'),
 
     /*
     |--------------------------------------------------------------------------
