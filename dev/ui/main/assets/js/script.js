0172acd6e8880b2324b4c592064941737fb6328f
khai | apikey:1b0dd90a412730e44d310c9311dc8e297af006c00015f3e87f8c47706999e9e6 <khai.nh@fsofts.com>
2024-10-13 01:37:28 +0700

    save

--- dev/ui/main/assets/js/script.js
+++ dev/ui/main/assets/js/script.js
@@ -1,6 +1,4 @@
-import Script from "./modules/_home copy"
-
 
 $(document).ready(function(){
-    new Script();
+
 })
--- public/ui/main/libraries/app.js
+++ public/ui/main/libraries/app.js
@@ -14,41 +14,41 @@ const savePNGButton = wrapper.querySelector("[data-action=save-png]");
 let undoData = [];
 const canvas = wrapper.querySelector("canvas");
 const signaturePad = new SignaturePad(canvas, {
-  // It's Necessary to use an opaque color when saving image as JPEG;
-  // this option can be omitted if only saving as PNG or SVG
-  backgroundColor: 'rgb(255, 255, 255)'
+// It's Necessary to use an opaque color when saving image as JPEG;
+// this option can be omitted if only saving as PNG or SVG
+backgroundColor: 'rgb(255, 255, 255)'
 });
 
 function randomColor() {
-  const r = Math.round(Math.random() * 255);
-  const g = Math.round(Math.random() * 255);
-  const b = Math.round(Math.random() * 255);
-  return `rgb(${r},${g},${b})`;
+const r = Math.round(Math.random() * 255);
+const g = Math.round(Math.random() * 255);
+const b = Math.round(Math.random() * 255);
+return `rgb(${r},${g},${b})`;
 }
 
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
 // This also causes canvas to be cleared.
 function resizeCanvas() {
-  // When zoomed out to less than 100%, for some very strange reason,
-  // some browsers report devicePixelRatio as less than 1
-  // and only part of the canvas is cleared then.
-  const ratio = Math.max(window.devicePixelRatio || 1, 1);
+// When zoomed out to less than 100%, for some very strange reason,
+// some browsers report devicePixelRatio as less than 1
+// and only part of the canvas is cleared then.
+const ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-  // This part causes the canvas to be cleared
-  canvas.width = canvas.offsetWidth * ratio;
-  canvas.height = canvas.offsetHeight * ratio;
-  canvas.getContext("2d").scale(ratio, ratio);
+// This part causes the canvas to be cleared
+canvas.width = canvas.offsetWidth * ratio;
+canvas.height = canvas.offsetHeight * ratio;
+canvas.getContext("2d").scale(ratio, ratio);
 
-  // This library does not listen for canvas changes, so after the canvas is automatically
-  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
-  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
-  // that the state of this library is consistent with visual state of the canvas, you
-  // have to clear it manually.
-  //signaturePad.clear();
+// This library does not listen for canvas changes, so after the canvas is automatically
+// cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+// canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+// that the state of this library is consistent with visual state of the canvas, you
+// have to clear it manually.
+//signaturePad.clear();
 
-  // If you want to keep the drawing on resize instead of clearing it you can reset the data.
-  signaturePad.fromData(signaturePad.toData());
+// If you want to keep the drawing on resize instead of clearing it you can reset the data.
+signaturePad.fromData(signaturePad.toData());
 }
 
 // On mobile devices it might make more sense to listen to orientation change,
@@ -68,44 +68,44 @@ resizeCanvas();
 // });
 
 function download(dataURL, filename) {
-  const blob = dataURLToBlob(dataURL);
-  const url = window.URL.createObjectURL(blob);
+const blob = dataURLToBlob(dataURL);
+const url = window.URL.createObjectURL(blob);
 
-  const a = document.createElement("a");
-  a.style = "display: none";
-  a.href = url;
-  a.download = filename;
+const a = document.createElement("a");
+a.style = "display: none";
+a.href = url;
+a.download = filename;
 
-  document.body.appendChild(a);
-  a.click();
+document.body.appendChild(a);
+a.click();
 
-  window.URL.revokeObjectURL(url);
+window.URL.revokeObjectURL(url);
 }
 
 // One could simply use Canvas#toBlob method instead, but it's just to show
 // that it can be done using result of SignaturePad#toDataURL.
 function dataURLToBlob(dataURL) {
-  // Code taken from https://github.com/ebidel/filer.js
-  const parts = dataURL.split(';base64,');
-  const contentType = parts[0].split(":")[1];
-  const raw = window.atob(parts[1]);
-  const rawLength = raw.length;
-  const uInt8Array = new Uint8Array(rawLength);
+// Code taken from https://github.com/ebidel/filer.js
+const parts = dataURL.split(';base64,');
+const contentType = parts[0].split(":")[1];
+const raw = window.atob(parts[1]);
+const rawLength = raw.length;
+const uInt8Array = new Uint8Array(rawLength);
 
-  for (let i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i);
-  }
+for (let i = 0; i < rawLength; ++i) {
+uInt8Array[i] = raw.charCodeAt(i);
+}
 
-  return new Blob([uInt8Array], { type: contentType });
+return new Blob([uInt8Array], { type: contentType });
 }
 
 signaturePad.addEventListener("endStroke", () => {
-  // clear undoData when new data is added
-  undoData = [];
+// clear undoData when new data is added
+undoData = [];
 });
 
 clearButton.addEventListener("click", () => {
-  signaturePad.clear();
+signaturePad.clear();
 });
 
 // undoButton.addEventListener("click", () => {
@@ -147,12 +147,12 @@ clearButton.addEventListener("click", () => {
 // });
 
 savePNGButton.addEventListener("click", () => {
-  if (signaturePad.isEmpty()) {
-    alert("Please provide a signature first.");
-  } else {
-    const dataURL = signaturePad.toDataURL();
-    download(dataURL, "signature.png");
-  }
+if (signaturePad.isEmpty()) {
+alert("Please provide a signature first.");
+} else {
+const dataURL = signaturePad.toDataURL();
+download(dataURL, "signature.png");
+}
 });
 
 // saveJPGButton.addEventListener("click", () => {
@@ -210,4 +210,3 @@ savePNGButton.addEventListener("click", () => {
 //     alert("Signature saved to localStorage!");
 //   }
 // });
-
